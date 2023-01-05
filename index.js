7b3ebaa14d0e2bcfac9c13fb97be7ef88eecd5bd
DipSapkota125 <dipsapkota325@gmail.com>
2023-01-05 16:20:04 +0545

    learning basic javascript

---
+++ index.js
@@ -0,0 +1,49 @@
+// var x = 9;
+// console.log(x);
+// x = 12;
+// console.log(x);
+// var x = 7 * 7;
+// console.log(x);
+
+// let y = 55;
+// console.log(y);
+// y = 56;
+// console.log(y);
+
+// let y = 90;
+// console.log(y);
+
+// const PI = 3.14;
+// console.log(PI);
+// PI = 3.89;
+// console.log(PI);
+// const PI = 90;
+
+// const x = 90;
+// console.log(x);
+// console.log(typeof x);
+
+// let name = "Amrit";
+// console.log(name);
+// console.log(typeof name);
+
+// let bool = true;
+// console.log(bool);
+// console.log(typeof bool);
+
+// let x = 90;
+// let y = 67;
+// let z = true;
+// let result = x + y + z;
+// console.log(result);
+
+// let name = "Amrit Gurung";
+// let y = 90;
+// let find = name + y;
+// console.log(find);
+
+const name = "Dip";
+const surname = "Sapkota";
+console.log("My name is : " + name + " " + surname + ".");
+const myGreeting = `My name is ${name} ${surname} .`;
+console.log(`Hello Everyone : ${myGreeting}`);
